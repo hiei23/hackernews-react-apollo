ffcc040d13389eaba7d2054cd37dc3bc42297eed
hiei23 <yuikazuga_25@hotmail.co.uk>
2018-02-08 11:37:26 -0500

    tutorial completed

---
+++ server/src/index.js
@@ -0,0 +1,32 @@
+const { GraphQLServer } = require('graphql-yoga')
+const { Prisma } = require('prisma-binding')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const Subscription = require('./resolvers/Subscription')
+const Feed = require('./resolvers/Feed')
+
+const resolvers = {
+  Query,
+  Mutation,
+  Subscription,
+  Feed,
+}
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: 'https://eu1.prisma.sh/public-sunfoe-485/hackernews-graphql-js/dev',
+      secret: 'mysecret123',
+    }),
+  }),
+})
+
+server.start(() => console.log('Server is running on http://localhost:4000'))
+
+
+const x = [1, 2, 3]
+const y = [...x, 4, 5, 6]
